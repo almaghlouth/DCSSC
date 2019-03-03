be2d844c87089a47ff55d2587736047e9cc3403d
Abdullah Al-Maghlouth <abdullah@maghlouth.com>
2019-03-03 23:58:04 +0300

    Base Contract Variables added

---
+++ DCSSC.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.5.1;
+
+contract DCSSC {
+    
+    address DCSSCowner;
+    address newDCSSC;
+    int256 commision;
+    //contentForSale
+    //contentMassSale
+    //itemForSale
+    struct Content {
+        string title;
+        string description;
+        int256 version;
+        string contentHash;
+        int256 serialNumber;
+        string link;
+        address creator;
+        address owner;
+        address publisher;
+        address seller;
+        int256 price;
+        int256 royality;
+    }
+    
+    Content[] Product;
+
+    
+    function() external payable {}
+    
+}
