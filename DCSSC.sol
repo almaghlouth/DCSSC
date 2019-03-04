d8f4353c2f3b129be091e85b279edeb7129d1fce
Abdullah Al-Maghlouth <abdullah@maghlouth.com>
2019-03-04 21:54:12 +0300

    added getItem and trackItem

--- DCSSC.sol
+++ DCSSC.sol
@@ -16,9 +16,9 @@ contract DCSSC {
         uint256 serialNumber;
         string link;
         address creator;
-        address owner;
         address publisher;
         address seller;
+        address owner;
         uint256 price;
         uint256 royality;
     }
@@ -29,12 +29,36 @@ contract DCSSC {
 
     function createItem (string memory _title, string memory _desc, uint _version, string memory _hash, string memory _link, uint _price, uint _royality) public returns (uint id, string memory title) {
         uint256 _count = contentCounter;
-        Content memory item = Content(_title,_desc,_version,_hash,0,_link,msg.sender,address(0),address(0),address(0),_price,_royality);
+        Content memory item = Content(_title,_desc,_version,_hash,0,_link,msg.sender,address(0),address(0),msg.sender,_price,_royality);
         product[_count][0] = item;
         contentCounter++;
         return (_count, product[_count][0].title);
     }
-
+    
+    function getItem (uint _id, uint _serial) public view returns (
+        string memory title,
+        string memory description,
+        uint256 version,
+        string memory contentHash,
+        uint256 serialNumber,
+        string memory link
+    ) {
+        return (product[_id][_serial].title, product[_id][_serial].description, product[_id][_serial].version, product[_id][_serial].contentHash,product[_id][_serial].serialNumber, product[_id][_serial].link);
+    }
+        
+    function trackItem (uint _id, uint _serial) public view returns (
+        address creator,
+        address publisher,
+        address seller,
+        address owner,
+        uint256 price,
+        uint256 royality
+    ) {
+        return (product[_id][_serial].creator, 
+            product[_id][_serial].publisher, product[_id][_serial].seller,product[_id][_serial].owner, product[_id][_serial].price, product[_id][_serial].royality);
+    }
+        
+    
     function() external payable {}
     
 }
