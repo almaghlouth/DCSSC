e7900dcd0821bf7d439d46106630d8f1bf7538ce
Abdullah Al-Maghlouth <abdullah@maghlouth.com>
2019-03-04 20:37:47 +0300

    add createItem function

--- DCSSC.sol
+++ DCSSC.sol
@@ -11,21 +11,30 @@ contract DCSSC {
     struct Content {
         string title;
         string description;
-        int256 version;
+        uint256 version;
         string contentHash;
-        int256 serialNumber;
+        uint256 serialNumber;
         string link;
         address creator;
         address owner;
         address publisher;
         address seller;
-        int256 price;
-        int256 royality;
+        uint256 price;
+        uint256 royality;
     }
     
-    Content[] Product;
-
+    uint contentCounter = 0;
     
+    mapping(uint => mapping (uint => Content)) product;
+
+    function createItem (string memory _title, string memory _desc, uint _version, string memory _hash, string memory _link, uint _price, uint _royality) public returns (uint id, string memory title) {
+        uint256 _count = contentCounter;
+        Content memory item = Content(_title,_desc,_version,_hash,0,_link,msg.sender,address(0),address(0),address(0),_price,_royality);
+        product[_count][0] = item;
+        contentCounter++;
+        return (_count, product[_count][0].title);
+    }
+
     function() external payable {}
     
 }
