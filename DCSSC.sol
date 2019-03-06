9385dbaf6e5112627ea0b91db537b1d41c38c3b7
Abdullah Al-Maghlouth <abdullah@maghlouth.com>
2019-03-07 02:10:47 +0300

    add migration and events

--- DCSSC.sol
+++ DCSSC.sol
@@ -42,6 +42,20 @@ contract DCSSC {
         _;
     }
     
+    event ContentPutForSale(uint id);
+    
+    event ContentAuthenticated(uint id);
+    
+    event ContentAwarded(uint id);
+    
+    event ContentMassSale(uint id);
+    
+    event ContentMassBought(uint id, uint quantity);
+    
+    event ItemPutForSale(uint id, uint serial);
+    
+    event ItemSold(uint id, uint serial);
+    
     constructor (uint _commision) public {
         DCSSCowner = msg.sender;
         newDCSSC = address(0);
@@ -51,11 +65,13 @@ contract DCSSC {
     mapping(uint => mapping (uint => Content)) product;
 
     function createItem (string memory _title, string memory _desc, uint _version, string memory _hash, string memory _link) public returns (uint id, uint serial, string memory title) {
+        if (newDCSSC != address(0)){
         uint256 _count = contentCounter;
         Content memory item = Content(_title,_desc,_version,_hash,0,_link,msg.sender,address(0),address(0),address(0),msg.sender,0);
         product[_count][0] = item;
         contentCounter++;
         return (_count, 0, product[_count][0].title);
+        }
     }
     
     function getItem (uint _id, uint _serial) public view returns (
@@ -66,7 +82,7 @@ contract DCSSC {
         uint256 serialNumber,
         string memory link
     ) {
-        return (product[_id][_serial].title, product[_id][_serial].description, product[_id][_serial].version, product[_id][_serial].contentHash,product[_id][_serial].serialNumber, product[_id][_serial].link);
+        return (product[_id][_serial].title, product[_id][_serial].description, product[_id][_serial].version, contentsForSale[_id].authHash,product[_id][_serial].serialNumber, product[_id][_serial].link);
     }
         
     function trackItem (uint _id, uint _serial) public view returns (
@@ -106,7 +122,6 @@ contract DCSSC {
         return newDCSSC;
     }
     
-    //add event
     function putContentForSale (uint _id, uint _price, uint _royality) public {
         if ((msg.sender == product[_id][0].creator) && (product[_id][0].publisher == address(0)) && (contentsForSale[_id].awarded == false) && (contentsForSale[_id].auction == false) ) {
             contentsForSale[_id].id = _id;
@@ -114,6 +129,7 @@ contract DCSSC {
             contentsForSale[_id].price = _price;
             contentsForSale[_id].royality = _royality;
             contentsForSale[_id].currentSerial = 1;
+            emit ContentPutForSale(_id);
         }
     }
     
@@ -129,29 +145,29 @@ contract DCSSC {
         return (contentsForSale[_id].id, contentsForSale[_id].auction,contentsForSale[_id].awarded,contentsForSale[_id].massSale,contentsForSale[_id].price,contentsForSale[_id].royality,contentsForSale[_id].authHash);
     }
     
-    //add event
     function authenticate (uint _id, string memory _hash) public {
-        if (keccak256(abi.encodePacked(_hash)) == keccak256(abi.encodePacked(product[_id][0].contentHash))){
+        if ((keccak256(abi.encodePacked(_hash)) == keccak256(abi.encodePacked(product[_id][0].contentHash))) && (product[_id][0].authenticator == address(0))){
             contentsForSale[_id].authHash = _hash;
             product[_id][0].authenticator = msg.sender;
+            emit ContentAuthenticated(_id);
         }
     }
     
-    //add event
     function buyContentForSale (uint _id) payable public  {
         if ((msg.value >= contentsForSale[_id].price) && (product[_id][0].publisher == address(0)) 
         && (contentsForSale[_id].awarded == false) && (contentsForSale[_id].auction == true) ) {
             product[_id][0].creator.transfer(msg.value);
             contentsForSale[_id].awarded = true;
-            product[_id][0].publisher = msg.sender; 
+            product[_id][0].publisher = msg.sender;
+            emit ContentAwarded(_id);
         }
     }
     
-    //add event
     function putMassSale (uint _id, uint _itemPrice) public {
-        if ((product[_id][0].publisher == msg.sender) && (contentsForSale[_id].awarded == true) && (contentsForSale[_id].massSale == false)) {
+        if ((product[_id][0].publisher == msg.sender) && (product[_id][0].authenticator != address(0)) && (contentsForSale[_id].awarded == true) && (contentsForSale[_id].massSale == false)) {
             product[_id][0].price = _itemPrice + contentsForSale[_id].royality + commision;
             contentsForSale[_id].massSale = true;
+            emit ContentMassSale(_id);
         }
     }
     
@@ -159,24 +175,24 @@ contract DCSSC {
         return (product[_id][0].price * _quantity);
     }
     
-    //add event
     function buyMassSale (uint _id, uint _quantity) public payable {
         if ((contentsForSale[_id].massSale == true) && (msg.value == estimateMassSale(_id,_quantity))) {
             product[_id][0].publisher.transfer(msg.value);
             massBuyStock[_id][msg.sender] = _quantity;
+            emit ContentMassBought(_id,_quantity);
         }
     }
     
-    //add event
     function sellItem (uint _id, uint _itemPrice) public returns (uint serialNumber) {
         if (massBuyStock[_id][msg.sender] > 0){
-            uint inc = contentsForSale[_id].currentSerial;
-            product[_id][inc] = product[_id][0];
-            product[_id][inc].seller = msg.sender;
-            product[_id][inc].price = _itemPrice;
+            uint _inc = contentsForSale[_id].currentSerial;
+            product[_id][_inc] = product[_id][0];
+            product[_id][_inc].seller = msg.sender;
+            product[_id][_inc].price = _itemPrice;
             contentsForSale[_id].currentSerial++;
             massBuyStock[_id][msg.sender]--;
-            return inc;
+            emit ItemPutForSale(_id,_inc);
+            return _inc;
         }
     }
     
@@ -187,6 +203,7 @@ contract DCSSC {
             product[_id][_serial].creator.transfer(contentsForSale[_id].royality);
             product[_id][_serial].seller.transfer(msg.value - commision - contentsForSale[_id].royality);
             product[_id][_serial].owner = msg.sender;
+            emit ItemSold(_id,_serial);
         }
     }
     
