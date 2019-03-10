0fe0d548a8f487e9bf8865854f881ebddf8b2456
Abdullah Al-Maghlouth <abdullah@maghlouth.com>
2019-03-10 22:49:29 +0300

    complete  mocha tests and minorfixes

--- contracts/DCSSC.sol
+++ contracts/DCSSC.sol
@@ -20,41 +20,41 @@ contract DCSSC {
         uint256 price;
     }
     
-    uint contentCounter = 0;
+    uint256 contentCounter = 0;
     
     struct contentForSale {
-        uint id;
+        uint256 id;
         bool auction;
         bool awarded;
         bool massSale;
-        uint price;
-        uint royality;
+        uint256 price;
+        uint256 royality;
         string authHash;
-        uint currentSerial;
+        uint256 currentSerial;
     }
     
-    mapping (uint => contentForSale) contentsForSale;
+    mapping (uint256 => contentForSale) contentsForSale;
     
-    mapping (uint => mapping (address => uint)) massBuyStock;
+    mapping (uint256 => mapping (address => uint)) massBuyStock;
     
     modifier onlyOwner {
         require(msg.sender == DCSSCowner);
         _;
     }
     
-    event ContentPutForSale(uint id);
+    event ContentPutForSale(uint256 id);
     
-    event ContentAuthenticated(uint id);
+    event ContentAuthenticated(uint256 id);
     
-    event ContentAwarded(uint id);
+    event ContentAwarded(uint256 id);
     
-    event ContentMassSale(uint id);
+    event ContentMassSale(uint256 id);
     
-    event ContentMassBought(uint id, uint quantity);
+    event ContentMassBought(uint256 id, uint256 quantity);
     
-    event ItemPutForSale(uint id, uint serial);
+    event ItemPutForSale(uint256 id, uint256 serial);
     
-    event ItemSold(uint id, uint serial);
+    event ItemSold(uint256 id, uint256 serial);
     
     constructor () public {
         DCSSCowner = msg.sender;
@@ -62,9 +62,9 @@ contract DCSSC {
         commision = 0;
     }
     
-    mapping(uint => mapping (uint => Content)) product;
+    mapping(uint256 => mapping (uint256 => Content)) product;
 
-    function createItem (string memory _title, string memory _desc, uint _version, string memory _hash, string memory _link) public returns (uint id, uint serial, string memory title) {
+    function createItem (string memory _title, string memory _desc, uint256 _version, string memory _hash, string memory _link) public returns (uint256 id, uint256 serial, string memory title) {
         if (newDCSSC == address(0)){
         uint256 _count = contentCounter;
         Content memory item = Content(_title,_desc,_version,_hash,0,_link,msg.sender,address(0),address(0),address(0),msg.sender,0);
@@ -74,7 +74,7 @@ contract DCSSC {
         }
     }
     
-    function getItem (uint _id, uint _serial) public view returns (
+    function getItem (uint256 _id, uint256 _serial) public view returns (
         string memory title,
         string memory description,
         uint256 version,
@@ -85,7 +85,7 @@ contract DCSSC {
         return (product[_id][_serial].title, product[_id][_serial].description, product[_id][_serial].version, contentsForSale[_id].authHash,product[_id][_serial].serialNumber, product[_id][_serial].link);
     }
         
-    function trackItem (uint _id, uint _serial) public view returns (
+    function trackItem (uint256 _id, uint256 _serial) public view returns (
         address creator,
         address authenticator,
         address publisher,
@@ -102,11 +102,11 @@ contract DCSSC {
         DCSSCowner = _address;
     }
     
-    function getDCSSCOwner () public view returns (address) {
+    function getDCSSCOwner () public view returns (address payable) {
         return DCSSCowner;
     }
     
-    function setCommision (uint _commision) onlyOwner public {
+    function setCommision (uint256 _commision) onlyOwner public {
         commision = _commision;
     }
     
@@ -122,7 +122,7 @@ contract DCSSC {
         return newDCSSC;
     }
     
-    function putContentForSale (uint _id, uint _price, uint _royality) public {
+    function putContentForSale (uint256 _id, uint256 _price, uint256 _royality) public {
         if ((msg.sender == product[_id][0].creator) && (product[_id][0].publisher == address(0)) && (contentsForSale[_id].awarded == false) && (contentsForSale[_id].auction == false) ) {
             contentsForSale[_id].id = _id;
             contentsForSale[_id].auction = true;
@@ -133,19 +133,20 @@ contract DCSSC {
         }
     }
     
-    function getContentForSale (uint _id) public view returns (
-        uint id,
+    function getContentForSale (uint256 _id) public view returns (
+        uint256 id,
         bool auction,
         bool awarded,
         bool massSale,
-        uint price,
-        uint royality,
+        uint256 price,
+        uint256 royality,
         string memory authHash
         ){
-        return (contentsForSale[_id].id, contentsForSale[_id].auction,contentsForSale[_id].awarded,contentsForSale[_id].massSale,contentsForSale[_id].price,contentsForSale[_id].royality,contentsForSale[_id].authHash);
+        return (contentsForSale[_id].id, contentsForSale[_id].auction,contentsForSale[_id].awarded,contentsForSale[_id].massSale,
+        contentsForSale[_id].price,contentsForSale[_id].royality,contentsForSale[_id].authHash);
     }
     
-    function authenticate (uint _id, string memory _hash) public {
+    function authenticate (uint256 _id, string memory _hash) public {
         if ((keccak256(abi.encodePacked(_hash)) == keccak256(abi.encodePacked(product[_id][0].contentHash))) && (product[_id][0].authenticator == address(0)) && (contentsForSale[_id].auction == true)){
             contentsForSale[_id].authHash = _hash;
             product[_id][0].authenticator = msg.sender;
@@ -153,47 +154,51 @@ contract DCSSC {
         }
     }
     
-    function buyContentForSale (uint _id) payable public  {
+    function buyContentForSale (uint256 _id) payable public returns (bool) {
         if ((msg.value >= contentsForSale[_id].price) && (product[_id][0].publisher == address(0)) 
         && (contentsForSale[_id].awarded == false) && (contentsForSale[_id].auction == true) ) {
             product[_id][0].creator.transfer(msg.value);
             contentsForSale[_id].awarded = true;
             product[_id][0].publisher = msg.sender;
             emit ContentAwarded(_id);
+            return true;
         }
+        return false;
     }
     
-    function putMassSale (uint _id, uint _itemPrice) public {
+    function putMassSale (uint256 _id, uint256 _itemPrice) public {
         if ((product[_id][0].publisher == msg.sender) && (product[_id][0].authenticator != address(0)) && (contentsForSale[_id].awarded == true) && (contentsForSale[_id].massSale == false)) {
-            product[_id][0].price = _itemPrice + contentsForSale[_id].royality + commision;
+            product[_id][0].price = _itemPrice;
             contentsForSale[_id].massSale = true;
             emit ContentMassSale(_id);
         }
     }
     
-    function estimateMassSale (uint _id, uint _quantity) public view returns (uint) {
-        return (product[_id][0].price * _quantity);
+    function estimateMassSale (uint256 _id, uint256 _quantity) public view returns (uint256) {
+        return ((product[_id][0].price) * (_quantity));
     }
 
-    function checkStock (uint _id, address _address) public view returns (uint) {
+    function checkStock (uint256 _id, address payable _address) public view returns (uint256) {
         return massBuyStock[_id][_address];
     }
     
-    function buyMassSale (uint _id, uint _quantity) public payable {
-        if ((contentsForSale[_id].massSale == true) && (msg.value == estimateMassSale(_id,_quantity))) {
+    function buyMassSale (uint256 _id, uint256 _quantity) public payable returns (bool) {
+        if ((contentsForSale[_id].massSale == true) && (estimateMassSale ( _id, _quantity) == msg.value)) {
             product[_id][0].publisher.transfer(msg.value);
             massBuyStock[_id][msg.sender] = _quantity;
             emit ContentMassBought(_id,_quantity);
+            return true;
         }
+        return false;
     }
     
-    function sellItem (uint _id, uint _itemPrice) public returns (uint serialNumber) {
-        if (massBuyStock[_id][msg.sender] > 0){
-            uint _inc = contentsForSale[_id].currentSerial;
+    function sellItem (uint256 _id, uint256 _itemPrice) public returns (uint256 serialNumber) {
+        if (checkStock(_id,msg.sender) > 0){
+            uint256 _inc = contentsForSale[_id].currentSerial;
             product[_id][_inc] = product[_id][0];
             product[_id][_inc].seller = msg.sender;
             product[_id][_inc].owner = address(0);
-            product[_id][_inc].price = _itemPrice;
+            product[_id][_inc].price = _itemPrice + commision + contentsForSale[_id].royality;
             contentsForSale[_id].currentSerial++;
             massBuyStock[_id][msg.sender]--;
             emit ItemPutForSale(_id,_inc);
@@ -201,7 +206,7 @@ contract DCSSC {
         }
     }
     
-    function buyItem (uint _id, uint _serial) public payable {
+    function buyItem (uint256 _id, uint256 _serial) public payable returns (bool){
         if ((_serial != 0) && (product[_id][_serial].owner == address(0)) && (product[_id][_serial].price == msg.value)
         && (contentsForSale[_id].massSale == true) && (product[_id][_serial].seller != address(0))  ){
             DCSSCowner.transfer(commision);
@@ -209,7 +214,9 @@ contract DCSSC {
             product[_id][_serial].seller.transfer(msg.value - commision - contentsForSale[_id].royality);
             product[_id][_serial].owner = msg.sender;
             emit ItemSold(_id,_serial);
+            return true;
         }
+        return false;
     }
     
     function() external payable {}
--- test/DCSSC.js
+++ test/DCSSC.js
@@ -1,132 +1,193 @@
 //import 'babel-polyfill';
-const DCSSC = artifacts.require('./DCSSC.sol')
+const DCSSC = artifacts.require("./DCSSC.sol");
 
-let instance;
-let accounts;
+contract("DCSSC", async accounts => {
+  //constructor
+  it("Constructor assigned contract creator as DCSSC owner", async () => {
+    let instance = await DCSSC.deployed();
+    assert.equal(await instance.getDCSSCOwner.call(), accounts[0]);
+  });
 
-contract('DCSSC', async(accs) => {
-   accounts = accs;
-   instance = await DCSSC.deployed();
+  //createItem
+  it("Can create Item", async () => {
+    let instance = await DCSSC.deployed();
+    await instance.createItem(
+      "Test Item",
+      "Item Description test",
+      1,
+      "sha256:8ACD0F91D1311BE5836078DDF9E1E2F8C2331A400E2A577F905B822444EB1F39",
+      "http://www.maghlouth.com/",
+      { from: accounts[1], gas: 1000000 }
+    );
+    let item = await instance.getItem.call(0, 0);
+    assert.equal(item[0], "Test Item");
+  });
 
-//constructor
-it('Constructor assigned contract creator as DCSSC owner', async() => {
-   assert.equal(await instance.getDCSSCOwner.call(), accounts[0])
+  //getItem
+  it("Can get Item", async () => {
+    let instance = await DCSSC.deployed();
+    let item = await instance.getItem.call(0, 0);
+    assert.equal(item[1], "Item Description test");
+  });
+
+  //trackItem
+  it("can track Item", async () => {
+    let instance = await DCSSC.deployed();
+    let item = await instance.trackItem.call(0, 0);
+    assert.equal(item[0], accounts[1]);
+  });
+
+  //getDCSSCOwner
+  it("Can get DCSSC Smart Contract Owner", async () => {
+    let instance = await DCSSC.deployed();
+    assert.equal(await instance.getDCSSCOwner.call(), accounts[0]);
+  });
+
+  //setDCSSCOwnerasOwner
+  it("Can set DCSSC smart contract owner by current owner", async () => {
+    let instance = await DCSSC.deployed();
+    await instance.setDCSSCOwner(accounts[7], {
+      from: accounts[0],
+      gas: 1000000
+    });
+    //console.log("current owner : " + (await instance.getDCSSCOwner.call()));
+    assert.equal(await instance.getDCSSCOwner.call(), accounts[7].toString());
+  });
+
+  //getCommision
+  it("Can get DCSSC smart contract commision", async () => {
+    let instance = await DCSSC.deployed();
+    assert.equal(await instance.getCommision.call(), 0);
+  });
+
+  //setCommisionasOwner
+  it("Can set DCSSC smart contract commision by current owner", async () => {
+    let instance = await DCSSC.deployed();
+    await instance.setCommision(50, { from: accounts[7], gas: 1000000 });
+    assert.equal(await instance.getCommision.call(), 50);
+  });
+
+  //getNewDCSSC
+  it("Can get new DCSSC smart contract address", async () => {
+    let instance = await DCSSC.deployed();
+    assert.equal(
+      await instance.getNewDCSSC.call(),
+      "0x0000000000000000000000000000000000000000"
+    );
+  });
+
+  //putContentForSale
+  it("Can put contents for sale", async () => {
+    let instance = await DCSSC.deployed();
+    await instance.putContentForSale(0, 1400, 50, {
+      from: accounts[1],
+      gas: 1000000
+    });
+    let item = await instance.getContentForSale.call(0);
+    assert.equal(await item[1], true);
+  });
+
+  //getContentForSale
+  it("Can get contents for sale", async () => {
+    let instance = await DCSSC.deployed();
+    let item = await instance.getContentForSale.call(0);
+    assert.equal(await item[1], true);
+  });
+
+  //authenticate
+  it("Can authenticate content", async () => {
+    let instance = await DCSSC.deployed();
+    await instance.authenticate(
+      0,
+      "sha256:8ACD0F91D1311BE5836078DDF9E1E2F8C2331A400E2A577F905B822444EB1F39",
+      { from: accounts[5], gas: 1000000 }
+    );
+    let obj = await instance.getContentForSale.call(0);
+    assert.equal(
+      await obj[6],
+      "sha256:8ACD0F91D1311BE5836078DDF9E1E2F8C2331A400E2A577F905B822444EB1F39"
+    );
+  });
+
+  //buyContentForSale
+  it("Can buy contents for sale", async () => {
+    let instance = await DCSSC.deployed();
+    await instance.buyContentForSale(0, {
+      from: accounts[2],
+      gas: 1000000,
+      value: 1400
+    });
+    let item = await instance.trackItem.call(0, 0);
+    assert.equal(await item[2], accounts[2]);
+  });
+
+  //putMassSale
+  it("Can put contents for mass sale", async () => {
+    let instance = await DCSSC.deployed();
+    await instance.putMassSale(0, 100, { from: accounts[2], gas: 1000000 });
+    let item = await instance.getContentForSale.call(0);
+    //console.log("mass Sale : " + (await JSON.stringify(item)));
+    assert.equal(await item[3], true);
+  });
+
+  //estimateMassSale
+  it("Can estimate mass sale costs", async () => {
+    let instance = await DCSSC.deployed();
+    let item = await instance.estimateMassSale(0, 5);
+    //console.log("Estimated value : " + (await item.toNumber()));
+    assert.equal(await item.toNumber(), 500);
+  });
+
+  //buyMassSale
+  it("Can mass buy contents from mass sale", async () => {
+    let instance = await DCSSC.deployed();
+    await instance.buyMassSale(0, 20, {
+      from: accounts[3],
+      gas: 1000000,
+      value: 2000
+    });
+    let item = await instance.checkStock(0, accounts[3].toString());
+    assert.equal(await item.toNumber(), 20);
+  });
+
+  //checkStock
+  it("Seller can check his own stock", async () => {
+    let instance = await DCSSC.deployed();
+    let item = await instance.checkStock(0, accounts[3].toString());
+    //console.log("Stock Found : " + (await item.toNumber()));
+    assert.equal(await item.toNumber(), 20);
+  });
+
+  //sellItem
+  it("Seller can put an item for sale", async () => {
+    let instance = await DCSSC.deployed();
+    await instance.sellItem(0, 200, {
+      from: accounts[3],
+      gas: 1000000
+    });
+    let item = await instance.trackItem.call(0, 1);
+    assert.equal(await item[3], accounts[3].toString());
+  });
+
+  //buyItem
+  it("Buyer can buy an item from sell list", async () => {
+    let instance = await DCSSC.deployed();
+    await instance.buyItem(0, 1, {
+      from: accounts[4],
+      gas: 1000000,
+      value: 300
+    });
+    let item = await instance.trackItem.call(0, 1);
+    assert.equal(await item[4], accounts[4].toString());
+  });
+
+  //setNewDCSSCasowner
+  it("Can set new DCSSC smart contract by current owner", async () => {
+    let instance = await DCSSC.deployed();
+    await instance.setNewDCSSC(accounts[9], {
+      from: accounts[7],
+      gas: 1000000
+    });
+    assert.equal(await instance.getNewDCSSC.call(), accounts[9].toString());
+  });
 });
-
-//createItem
-it('Can create Item', async() => {
-   await instance.createItem('Test Item','Item Description test',1,'sha256:8ACD0F91D1311BE5836078DDF9E1E2F8C2331A400E2A577F905B822444EB1F39','http://www.maghlouth.com/', { from: accounts[1] })
-   assert.equal(await instance.getItem.call(0,0)[0], 'Test Item')
-});
-
-//getItem
-it('Can get Item', async() => {
-   await instance.createItem('Test Item','Item Description test',1,'sha256:8ACD0F91D1311BE5836078DDF9E1E2F8C2331A400E2A577F905B822444EB1F39','http://www.maghlouth.com/', { from: accounts[1] })
-   assert.equal(await instance.getItem.call(0,0)[1], 'Item Descreption test')
-});
-
-//trackItem
-it('can track Item', async() => {
-   await instance.createItem('Test Item','Item Description test',1,'sha256:8ACD0F91D1311BE5836078DDF9E1E2F8C2331A400E2A577F905B822444EB1F39','http://www.maghlouth.com/', { from: accounts[1] })
-   assert.equal(await instance.trackItem.call(0,0)[0], accounts[1])
-});
-
-//getDCSSCOwner
-it('Can get DCSSC Smart Contract Owner', async() => {
-   assert.equal(await instance.getDCSSCOwner.call(), accounts[0])
-});
-
-//setDCSSCOwnerasOther
-it('Cannot set DCSSC smart contract owner without being the current owner', async() => {
-   await instance.setDCSSCOwnert(accounts[2],{ from: accounts[1] })
-   assert.equal(await instance.getDCSSCOwner.call(), accounts[0])
-});
-
-//setDCSSCOwnerasOwner
-it('Can set DCSSC smart contract owner by current owner', async() => {
-   await instance.setDCSSCOwnert(accounts[2],{ from: accounts[0] })
-   assert.equal(await instance.getDCSSCOwner.call(), accounts[2])
-});
-
-//getCommision
-it('Can get DCSSC smart contract commision', async() => {
-   assert.equal(await instance.getCommision.call(), 0)
-});
-
-//setCommisionasOther
-it('Cannot set DCSSC smart contract commision without being the current owner', async() => {
-   await instance.setCommision(60000,{ from: accounts[1] })
-   assert.equal(await instance.getCommision.call(), 0)
-});
-
-//setCommisionasOwner
-it('Can set DCSSC smart contract commision by current owner', async() => {
-   await instance.setCommision(90000,{ from: accounts[2] })
-   assert.equal(await instance.getCommision.call(), 90000)
-});
-
-//getNewDCSSC
-it('Can get new DCSSC smart contract address', async() => {
-   assert.equal(await instance.getNewDCSSC.call(), '0x0')
-});
-
-//setNewDCSSCasOther
-it('Cannot set DCSSC smart contract commision without being the current owner', async() => {
-   await instance.getNewDCSSC(accounts[6],{ from: accounts[5] })
-   assert.equal(await instance.getNewDCSSC.call(), '0x0')
-});
-
-//putContentForSale
-it('Can put contents for sale', async() => {
-   await instance.putContentForSale(0,5000000,30000, { from: accounts[1] })
-   assert.equal(await instance.getContentForSale.call(0)[1], true)
-});
-
-//getContentForSale
-it('Can get contents for sale', async() => {
-   assert.equal(await instance.getContentForSale.call(0)[1], true)
-});
-
-//authenticate
-it('Can authenticate content', async() => {
-   await instance.authenticate(0,'sha256:8ACD0F91D1311BE5836078DDF9E1E2F8C2331A400E2A577F905B822444EB1F39', { from: accounts[5] })
-   assert.equal(await instance.getContentForSale.call(0)[6], 'sha256:8ACD0F91D1311BE5836078DDF9E1E2F8C2331A400E2A577F905B822444EB1F39')
-});
-
-//buyContentForSale
-it('Can buy contents for sale', async() => {
-   await instance.putMassSale(0, { from: accounts[2], gas: 1000000, value: 5000000 })
-   assert.equal(await instance.trackItem.call(0,0)[8], accounts[2])
-});
-
-//putMassSale
-it('Can put contents for mass sale', async() => {
-   await instance.putContentForSale(0,100000, { from: accounts[2] })
-   assert.equal(await instance.getContentForSale.call(0)[3], true)
-});
-
-//estimateMassSale
-it('Can estimate mass sale costs', async() => {
-   assert.equal(await instance.estimateMassSale(0,4), 400000)
-});
-
-//buyMassSale
-it('Can mass buy contents through mass sale', async() => {
-   await instance.buyContentForSale(0,20, { from: accounts[3], gas: 1000000, value: 2000000 })
-   assert.equal(await instance.massBuyStock[0][accounts[3]], 20)
-});
-
-//sellItem
-it('Can mass buy contents through mass sale', async() => {
-   await instance.sellItem(0,200000, { from: accounts[3]})
-   assert.equal(await instance.trackItem.call(0,1)[9], accounts[3])
-});
-
-//buyItem
-it('Can buy item from sell list', async() => {
-   await instance.buyItem(0,1, { from: accounts[4], gas: 1000000, value: 320000 })
-   assert.equal(await instance.trackItem.call(0,1)[10], accounts[4])
-});
-
-});
-
